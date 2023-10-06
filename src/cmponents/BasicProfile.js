8bb3c9a5dbb05e5bef104992eed7393c41d104d7
kushal1997 <kushalrao103@gmail.com>
2023-10-06 22:38:06 +0530

    test to clear out netfliy shown warning

--- src/cmponents/BasicProfile.js
+++ src/cmponents/BasicProfile.js
@@ -4,16 +4,17 @@ const BasicProfile = () => {
   return (
     <Fragment>
       <div className="flex flex-row">
-        <a href="">
+        <a href="#">
           <img
             src="https://randomuser.me/api/portraits/men/32.jpg"
+            alt='men'
             width="100"
             className="rounded-full"
           />
         </a>
         <div className="w-72 pl-2 m-auto">
           <div className="text-sm font-medium">
-            <a href="">gigo2345</a>
+            <a href="#">gigo2345</a>
           </div>
           <div className="text-gray-500 text-sm">
             kushal rao | front end web
@@ -21,7 +22,7 @@ const BasicProfile = () => {
         </div>
         <div className="w-32 text-right m-auto ">
           <a
-            href=""
+            href="#"
             className="text-xs text-sky-500 font-bold cursor-pointer"
           >
             Sign Out
